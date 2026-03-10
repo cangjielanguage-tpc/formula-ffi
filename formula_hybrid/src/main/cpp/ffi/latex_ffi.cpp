554c1a272186eab033fc1afff1da1e53e7157042
caizhao <17749038935@163.com>
2026-03-10 01:00:20 -0800

    add unnormal latex and exception detial info on hybrid 5.1.1

--- formula_hybrid/src/main/cpp/ffi/latex_ffi.cpp
+++ formula_hybrid/src/main/cpp/ffi/latex_ffi.cpp
@@ -8,9 +8,26 @@
 
 #include <ctype.h>
 #include <string>
+#include <cstring>
 
 using namespace std;
 using namespace tex;
+
+#define TEX_ERROR_SUCCESS          0
+#define TEX_ERROR_SYNTAX           1
+#define TEX_ERROR_INVALID_MATRIX   2
+#define TEX_ERROR_INVALID_DELIM    3
+#define TEX_ERROR_TEX              4
+#define TEX_ERROR_UNKNOWN          99
+
+static void copyErrorMessage(char* dest, const std::string& src, int destSize) {
+    if (destSize <= 0) return;
+    strncpy(dest, src.c_str(), destSize - 1);
+    dest[destSize - 1] = '\0';
+}
+
+static void logError(const char* message, int errorCode, const char* formula) {
+}
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,6 +54,64 @@ TeXRender *LaTeX_parse(char *ltx, int width, float textSize, float lineSpace, ui
         TeXRender *r = LaTeX::parse(value, width, textSize, lineSpace, foreground);
         return r;
     } catch (exception& e) {
+        return nullptr;
+    }
+}
+
+TeXRender* LaTeX_parse_with_error(char *ltx, int width, float textSize, 
+                                   float lineSpace, uint32_t foreground,
+                                   int *errorCode, char *errorMsg, 
+                                   int errorMsgSize) {
+    if (errorCode == nullptr || errorMsg == nullptr || errorMsgSize <= 0) {
+        return nullptr;
+    }
+    
+    *errorCode = TEX_ERROR_SUCCESS;
+    errorMsg[0] = '\0';
+    
+    if (ltx == nullptr) {
+        *errorCode = TEX_ERROR_UNKNOWN;
+        strncpy(errorMsg, "Input formula is null", errorMsgSize - 1);
+        errorMsg[errorMsgSize - 1] = '\0';
+        return nullptr;
+    }
+    
+    wstring value;
+    try {
+        value = utf82wide(ltx);
+    } catch (exception& e) {
+        *errorCode = TEX_ERROR_UNKNOWN;
+        strncpy(errorMsg, "Invalid UTF-8 encoding", errorMsgSize - 1);
+        errorMsg[errorMsgSize - 1] = '\0';
+        return nullptr;
+    }
+    
+    try {
+        LaTeXParseResult parseResult = LaTeX::parseWithError(value, width, textSize, lineSpace, foreground);
+        
+        if (parseResult.success) {
+            TeXRender* render = parseResult.render;
+            parseResult.render = nullptr;
+            return render;
+        } else {
+            string msg = parseResult.errorMessage;
+            if (msg.find("invalid matrix") != string::npos || msg.find("column") != string::npos) {
+                *errorCode = TEX_ERROR_INVALID_MATRIX;
+            } else if (msg.find("delimiter") != string::npos || msg.find("bracket") != string::npos) {
+                *errorCode = TEX_ERROR_INVALID_DELIM;
+            } else if (msg.find("unknown") != string::npos || msg.find("Undefined") != string::npos) {
+                *errorCode = TEX_ERROR_TEX;
+            } else {
+                *errorCode = TEX_ERROR_SYNTAX;
+            }
+            copyErrorMessage(errorMsg, msg, errorMsgSize);
+            logError(msg.c_str(), *errorCode, ltx);
+            return nullptr;
+        }
+    } catch (exception& e) {
+        *errorCode = TEX_ERROR_UNKNOWN;
+        copyErrorMessage(errorMsg, e.what(), errorMsgSize);
+        logError(e.what(), TEX_ERROR_UNKNOWN, ltx);
         return nullptr;
     }
 }
