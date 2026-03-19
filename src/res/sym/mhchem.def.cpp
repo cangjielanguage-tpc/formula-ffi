16f3845dd56b03badf3e4763f2b90ef597367341
root <root@013001872561.isoftstone.com>
2026-03-19 11:18:46 +0800

    新增8种箭头类宏的的渲染

--- src/res/sym/mhchem.def.cpp
+++ src/res/sym/mhchem.def.cpp
@@ -6,5 +6,9 @@ E(35, 58376, longrightleftharpoons)
 E(35, 58377, longRightleftharpoons)
 E(35, 58378, longLeftrightharpoons)
 E(35, 58411, longleftrightarrows)
+E(35, 58384, tripledash)
+E(35, 58385, mhchemBondTD)
+E(35, 58386, mhchemBondTDD)
+E(35, 58387, mhchemBondDTD)
 
 END_DEF_SYMBOLS
